6293dd130944623f2f6ccfa33fe723109caef1c3
Taylor E <tjennen480@icloud.com>
2024-04-02 08:29:07 -0700

    bot updated with todo

--- bot.py
+++ bot.py
@@ -18,18 +18,13 @@ import torch
 import asyncio
 
 
-
-
-        
-
 # setting up the bot
 config = configparser.ConfigParser()
 config.read("config.properties")
 TOKEN = config["DISCORD"]["TOKEN"]
 IMAGE_SOURCE = config["IMAGE"]["SOURCE"]
-stripe_api_key=config["STRIPE"]["API_KEY"]  
+stripe_api_key = config["STRIPE"]["API_KEY"]
 stripe_product_id = config.get("STRIPE", "PRODUCT_ID")
-
 
 
 stripe.api_key = stripe_api_key
@@ -44,17 +39,16 @@ if IMAGE_SOURCE == "LOCAL":
     from imageGen import generate_images, upscale_image, generate_alternatives
 
 
-
-
-
-
-model = VisionEncoderDecoderModel.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
+model = VisionEncoderDecoderModel.from_pretrained(
+    "nlpconnect/vit-gpt2-image-captioning"
+)
 processor = ViTImageProcessor.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
 tokenizer = AutoTokenizer.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
 
 # Move the model to the GPU if available
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model.to(device)
+
 
 async def generate_caption(image):
     # Convert the image to RGB format
@@ -70,17 +64,16 @@ async def generate_caption(image):
     return caption
 
 
-
-
 async def extract_index_from_id(custom_id):
     try:
         # Extracting numeric part while assuming it might contain non-numeric characters
-        numeric_part = ''.join([char for char in custom_id[1:] if char.isdigit()])
+        numeric_part = "".join([char for char in custom_id[1:] if char.isdigit()])
         if not numeric_part:
             return None
         return int(numeric_part) - 1
     except ValueError:
         return None
+
 
 class ImageButton(discord.ui.Button):
     def __init__(self, label, emoji, row, callback):
@@ -94,7 +87,17 @@ class ImageButton(discord.ui.Button):
 
 
 class Buttons(discord.ui.View):
-    def __init__(self, prompt, negative_prompt, UUID, user_id, url, model, images, timeout=10000000000000):
+    def __init__(
+        self,
+        prompt,
+        negative_prompt,
+        UUID,
+        user_id,
+        url,
+        model,
+        images,
+        timeout=10000000000000,
+    ):
         super().__init__(timeout=timeout)
         self.UUID = UUID  # Store the UUID
         self.prompt = prompt
@@ -103,10 +106,10 @@ class Buttons(discord.ui.View):
         self.url = url
         self.model = model
         self.images = images
-    
 
-
-        total_buttons = len(self.images) * 2 + 1  # For both alternative and upscale buttons + re-roll button
+        total_buttons = (
+            len(self.images) * 2 + 1
+        )  # For both alternative and upscale buttons + re-roll button
         if total_buttons > 25:  # Limit to 25 buttons
             self.images = self.images[:12]  # Adjust to only use the first 12 images
 
@@ -135,11 +138,14 @@ class Buttons(discord.ui.View):
             cursor = conn.cursor()
 
             try:
-                cursor.execute("""
+                cursor.execute(
+                    """
                     SELECT * FROM images
                     WHERE UUID = ? AND user_id = ?
                     ORDER BY count
-                """, (UUID, user_id))
+                """,
+                    (UUID, user_id),
+                )
                 images = cursor.fetchall()
                 return images
 
@@ -156,55 +162,65 @@ class Buttons(discord.ui.View):
                 conn.close()
 
         images = await asyncio.to_thread(get_images)
+
         return cls(prompt, negative_prompt, UUID, user_id, url, model, images)
 
-    
-    async def reroll_image(self, interaction: discord.Interaction, button: discord.ui.Button, count: int):
+    async def reroll_image(
+        self, interaction: discord.Interaction, button: discord.ui.Button, count: int
+    ):
         try:
             batch_size = 4
             # Grab the button number and then convert that to count to grab with the UUID from the db
             index = await extract_index_from_id(button.custom_id)
             if index is None:
-                await interaction.response.send_message("Invalid custom_id format. Please ensure it contains a numeric index.")
+                await interaction.response.send_message(
+                    "Invalid custom_id format. Please ensure it contains a numeric index."
+                )
                 return
 
             await interaction.response.send_message(
-                f'{interaction.user.mention} asked me to re-imagine the image, this shouldn\'t take too long...'
+                f"{interaction.user.mention} asked me to re-imagine the image, this shouldn't take too long..."
             )
-            #credit check
+            # credit check
             user_id = interaction.user.id
             username = interaction.user.name
             user_credits = await discord_balance_prompt(user_id, username)
             if user_credits is None:
-               #add them to the db 
-                username=interaction.user.name
-                user_id=interaction.user.id
-                create_DB_user(user_id, username) 
+                # add them to the db
+                username = interaction.user.name
+                user_id = interaction.user.id
+                create_DB_user(user_id, username)
             elif user_credits < 5:  # Assuming 5 credits are needed
-                payment_link = await discord_recharge_prompt(interaction.user.name, self.user_id)
+                payment_link = await discord_recharge_prompt(
+                    interaction.user.name, self.user_id
+                )
                 if payment_link == "failed":
                     await interaction.response.send_message(
                         "Failed to create payment link or payment itself failed. Please try again later.",
-                        ephemeral=True
+                        ephemeral=True,
                     )
                     return
                 await interaction.response.send_message(
                     f"You don't have enough credits. Please recharge your account: {payment_link}",
-                    ephemeral=True
+                    ephemeral=True,
                 )
-                
 
             # Retrieve the prompt from the database
             conn = sqlite3.connect(DATABASE_URL)
             cursor = conn.cursor()
 
             try:
-                cursor.execute("""
+                cursor.execute(
+                    """
                     SELECT prompt FROM images
                     WHERE UUID = ? AND COUNT = ? LIMIT 1
-                """, (self.UUID, count))
+                """,
+                    (self.UUID, count),
+                )
                 result = cursor.fetchone()
-                prompt = result[0] if result else self.prompt  # Use the original prompt as a fallback
+                prompt = (
+                    result[0] if result else self.prompt
+                )  # Use the original prompt as a fallback
 
                 # Generate a new UUID for the re-rolled image
                 new_UUID = str(uuid.uuid4())
@@ -230,9 +246,16 @@ class Buttons(discord.ui.View):
                 await interaction.channel.send(
                     content=final_message,
                     file=discord.File(fp=collage_path, filename="collage.png"),
-                    view=Buttons(prompt, self.negative_prompt, new_UUID, interaction.user.id, collage_path, self.model)
+                    view=Buttons(
+                        prompt,
+                        self.negative_prompt,
+                        new_UUID,
+                        interaction.user.id,
+                        collage_path,
+                        self.model,
+                    ),
                 )
-            #after successful reroll, deduct credits
+                # after successful reroll, deduct credits
                 amount = user_credits - 10
                 await deduct_credits(user_id, amount)
 
@@ -248,59 +271,70 @@ class Buttons(discord.ui.View):
 
         except discord.errors.InteractionResponded:
             return print("interaction failed")
-        
-    
-    async def upscale_image(self, interaction: discord.Interaction, button: discord.ui.Button, count: int):
+
+    async def upscale_image(
+        self, interaction: discord.Interaction, button: discord.ui.Button, count: int
+    ):
         try:
             index = await extract_index_from_id(button.custom_id)
             if index is None:
-                await interaction.response.send_message("Invalid custom_id format. Please ensure it contains a numeric index.")
+                await interaction.response.send_message(
+                    "Invalid custom_id format. Please ensure it contains a numeric index."
+                )
                 return
 
             await interaction.response.send_message(
-                f'{interaction.user.mention} asked me to upscale the image, this shouldn\'t take too long...'
+                f"{interaction.user.mention} asked me to upscale the image, this shouldn't take too long..."
             )
             # check credits
             user_id = interaction.user.id
             username = interaction.user.name
             user_credits = await discord_balance_prompt(user_id, username)
             if user_credits is None:
-               #add them to the db 
-                username=interaction.user.name
-                user_id=interaction.user.id
-                create_DB_user(user_id, username) 
-               
+                # add them to the db
+                username = interaction.user.name
+                user_id = interaction.user.id
+                create_DB_user(user_id, username)
+
             elif user_credits < 5:  # Assuming 5 credits are needed
-                payment_link = await discord_recharge_prompt(interaction.user.name, self.user_id)
+                payment_link = await discord_recharge_prompt(
+                    interaction.user.name, self.user_id
+                )
                 if payment_link == "failed":
                     await interaction.response.send_message(
                         "Failed to create payment link or payment itself failed. Please try again later.",
-                        ephemeral=True
+                        ephemeral=True,
                     )
                     return
                 await interaction.response.send_message(
                     f"You don't have enough credits. Please recharge your account: {payment_link}",
-                    ephemeral=True
+                    ephemeral=True,
                 )
                 return
-            
 
             conn = sqlite3.connect(DATABASE_URL)
             cursor = conn.cursor()
 
             try:
-                cursor.execute("""
+                cursor.execute(
+                    """
                     SELECT prompt FROM images
                     WHERE UUID = ? AND COUNT = ? LIMIT 1
-                """, (self.UUID,))
+                """,
+                    (self.UUID,),
+                )
                 images = cursor.fetchall()
 
                 if index < len(images):
                     image = images[index]
-                    image_data = image[2]  # Assuming image data is stored in the 3rd column
+                    image_data = image[
+                        2
+                    ]  # Assuming image data is stored in the 3rd column
 
                     # Upscale image logic assumed to be defined elsewhere
-                    upscaled_image = await upscale_image(image_data, self.prompt, self.negative_prompt)
+                    upscaled_image = await upscale_image(
+                        image_data, self.prompt, self.negative_prompt
+                    )
 
                     timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
                     upscaled_image_path = f"./out/upscaledImage_{timestamp}.png"
@@ -308,13 +342,17 @@ class Buttons(discord.ui.View):
                     # Assuming upscaled_image has a .save() method
                     upscaled_image.save(upscaled_image_path)
 
-                    final_message = f"{interaction.user.mention} here is your upscaled image"
+                    final_message = (
+                        f"{interaction.user.mention} here is your upscaled image"
+                    )
                     await interaction.channel.send(
                         content=final_message,
-                        file=discord.File(fp=upscaled_image_path, filename="upscaled_image.png")
+                        file=discord.File(
+                            fp=upscaled_image_path, filename="upscaled_image.png"
+                        ),
                     )
-                    #deduct credits
-                    amount = user_credits - 5 
+                    # deduct credits
+                    amount = user_credits - 5
                     print(amount)
                     await deduct_credits(user_id, amount)
                 else:
@@ -332,7 +370,7 @@ class Buttons(discord.ui.View):
 
         except discord.errors.InteractionResponded:
             print("Interaction already responded")
-        
+
 
 @client.event
 async def on_ready():
@@ -357,14 +395,19 @@ async def describe(interaction: discord.Interaction, image: discord.Attachment):
         # Send the generated caption as a response
         await interaction.response.send_message(content=caption)
     else:
-        await interaction.response.send_message("Please provide a valid image attachment.")
+        await interaction.response.send_message(
+            "Please provide a valid image attachment."
+        )
+
 
 @tree.command(name="imagine", description="Generate an image based on input text")
 @app_commands.describe(prompt="Prompt for the image being generated")
-@app_commands.describe(negative_prompt="Prompt for what you want to steer the AI away from")
-@app_commands.describe(batch_size="Number of images to generate" )
+@app_commands.describe(
+    negative_prompt="Prompt for what you want to steer the AI away from"
+)
+@app_commands.describe(batch_size="Number of images to generate")
 @app_commands.describe(width="width of the image")
-@app_commands.describe(height="height of the image" )
+@app_commands.describe(height="height of the image")
 @app_commands.describe(model="model to use")
 @app_commands.describe(attachment="attachment to use")
 @app_commands.describe(lora="predefined loras to call from")
@@ -379,42 +422,39 @@ async def imagine(
     attachment: Optional[discord.Attachment] = None,
     lora: Optional[str] = None,
 ):
+    ## TODO: package parameters into a dataclass and build functions around it.
+    
     username = interaction.user.name
     user_id = interaction.user.id
 
     user_credits = await discord_balance_prompt(user_id, username)
-    
 
     if user_credits is None:
         # Handle case where user credits couldn't be retrieved
-        create_DB_user(user_id, username) 
+        create_DB_user(user_id, username)
 
     elif user_credits < 10:  # Assuming 5 credits are needed
         payment_link = await discord_recharge_prompt(username, user_id)
         if payment_link == "failed":
             await interaction.response.send_message(
                 "Failed to create payment link or payment itself failed. Please try again later.",
-                ephemeral=True
+                ephemeral=True,
             )
-           
+
         await interaction.response.send_message(
             f"You don't have enough credits. Please recharge your account: {payment_link}",
-            ephemeral=True
+            ephemeral=True,
         )
         await interaction.response.defer(ephemeral=True)
     else:
         await interaction.response.defer(ephemeral=False)
 
-
-
-    
-
     UUID = str(uuid.uuid4())  # Generate unique hash for each image
     prompt_gen = f"{prompt}, masterpiece, best quality"
 
     if attachment:
-        #save input image 
-        
+        # save input image
+
         await style_images(
             UUID=UUID,
             user_id=interaction.user.id,
@@ -440,52 +480,64 @@ async def imagine(
         )
 
     collage_path = await create_collage(UUID)
+
     if collage_path is None:
         print(collage_path)
-        return 
-    buttons_view = await Buttons.create(prompt, negative_prompt, UUID, interaction.user.id, collage_path, model)
+        return
+
+    buttons_view = await Buttons.create(
+        prompt,
+        negative_prompt,
+        UUID,
+        interaction.user.id,
+        collage_path,
+        model,
+        batch_size,
+    )
 
     file = discord.File(collage_path, filename="collage.png")
     final_message = f'{interaction.user.mention}, here is what I imagined for you with "{prompt}", "{model}":'
-    
-    await interaction.followup.send(content=final_message, file=file, view=buttons_view, ephemeral=False)
+
+    await interaction.followup.send(
+        content=final_message, file=file, view=buttons_view, ephemeral=False
+    )
+
     if user_id == "879714655356997692":
         print("User ID matches the specified value. Skipping credit deduction.")
     else:
         amount = user_credits - 10
         print(amount)
         await deduct_credits(user_id, amount)
-        
-    
-    return UUID 
+
+    return UUID
+
 
 @tree.command(name="recharge", description="Recharge credits with Stripe")
 async def recharge(
     interaction: discord.Interaction,
-
 ):
     user_id = interaction.user.id
     username = interaction.user.name
 
-    #Make sure they exist in the db
+    # Make sure they exist in the db
     user_credits = await discord_balance_prompt(user_id, username)
     if user_credits is None:
-        create_DB_user(user_id, username) 
+        create_DB_user(user_id, username)
 
-    payment_link = await create_payment_link(user_id, await get_default_pricing(stripe_product_id))
+    payment_link = await create_payment_link(
+        user_id, await get_default_pricing(stripe_product_id)
+    )
     if payment_link == "failed":
         await interaction.response.send_message(
             f"Failed to create payment link or payment itself failed. Please try again later.",
-            ephemeral=True
+            ephemeral=True,
         )
         exit
     await interaction.response.send_message(
-        f"Recharge your account: {payment_link}",
-        ephemeral=True
+        f"Recharge your account: {payment_link}", ephemeral=True
     )
     await interaction.response.defer(ephemeral=True)
-   
-    
+
 
 @tree.command(name="balance", description="Check your credit balance")
 async def balance(
@@ -493,16 +545,16 @@ async def balance(
 ):
     user_id = interaction.user.id
     username = interaction.user.name
-    # make sure they exist in the db 
+    # make sure they exist in the db
     user_credits = await discord_balance_prompt(user_id, username)
     if user_credits is None:
-        create_DB_user(user_id, username) 
+        create_DB_user(user_id, username)
 
     await interaction.response.send_message(
-        f"Your current balance is: {user_credits}",
-        ephemeral=True
+        f"Your current balance is: {user_credits}", ephemeral=True
     )
     await interaction.response.defer(ephemeral=True)
+
 
 def generate_bot_invite_link(client_id):
     base_url = "https://discord.com/api/oauth2/authorize"
@@ -512,7 +564,6 @@ def generate_bot_invite_link(client_id):
         f"{base_url}?client_id={client_id}&permissions={permissions}&scope={scope}"
     )
     return invite_link
-
 
 
 # Example usage
